dc04f873766655dcddaf2920eae1ab1154940fe1
mayowadabiri <mayowad43@gmail.com>
2026-01-10 16:23:26 +0100

    added commander packahe

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,2 +1,9 @@
-// CLI entry point
-console.log("MirrorDB CLI");
+import { createProgram } from "./program.js";
+
+try {
+  const program = createProgram();
+  program.parse(process.argv);
+} catch (error) {
+  console.error("Unexpected error:", error);
+  process.exit(1);
+}
